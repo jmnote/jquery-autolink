045ee423c2c5bfa47458eb38e2f43b5b3b2b52aa
jmnote <jmnote@users.noreply.github.com>
2017-06-17 22:01:06 +0900

    .

--- jquery.autolink.js
+++ jquery.autolink.js
@@ -1,9 +1,7 @@
 (function($) {
-	$.fn.autolink = function (target) {
-		if (target == null) target = '_parent';
+	$.fn.autolink = function() {
 		return this.each( function(){
-			var re = /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
-			$(this).html( $(this).html().replace(re, '<a href="$1" target="'+ target +'">$1</a> ') );
+			$(this).html( $(this).html().replace(/((http|https|ftp):\/\/([^\s]+))/g,'<a href="$1" target="_blank">$1</a>') );
 		});
 	}
 })(jQuery);
